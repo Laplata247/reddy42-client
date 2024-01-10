0fff0b43777918dd8dbfc9a3251a38f898e11294
asiek728 <asiek728@gmail.com>
2024-01-10 16:03:34 +0000

    add navigation bar tests

--- client_painmap/src/pages/MapPainPage/index.jsx
+++ client_painmap/src/pages/MapPainPage/index.jsx
@@ -1,41 +1,60 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import html2canvas from 'html2canvas';
 import Model from '../../components/model.jsx'; // Import the Model component
+import './style.css';
 
 const MapPainPage = () => {
+
+  const takeScreenshot=() => {
+    const element = document.getElementById("canvasDiv")
+    html2canvas(element).then((canvas)=> {
+      let image = canvas.toDataURL("image/jpeg");
+      const a = document.createElement("a")
+      a.href = image
+      a.download = "screenshot.jpeg"
+      a.click()
+    })
+  }
+
   return (
     <>
       <h1>Map Pain Page</h1>
 
-      <Canvas
-        camera={{
-          position: [0, 2, 8], // Camera position
-          fov: 45,
-          near: 0.1,
-          far: 100,
-        }}
-        shadowMap={true} // Re-enable shadow mapping
-        style={{
-          height: '100vh',
-          width: '100vw',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <ambientLight intensity={0.5} />
-        <directionalLight
-          position={[5, 5, 5]}
-          intensity={1}
-          castShadow // Enable shadow casting
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-        />
-        <Model />
+      <div id='canvasDiv'>
+        <Canvas
+          camera={{
+            position: [0, 2, 8], // Camera position
+            fov: 45,
+            near: 0.1,
+            far: 100,
+          }}
+          shadowMap={true} // Re-enable shadow mapping
+          style={{
+            height: '60vh',
+            width: '70vw',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <ambientLight intensity={0.5} />
+          <directionalLight
+            position={[5, 5, 5]}
+            intensity={1}
+            castShadow // Enable shadow casting
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          <Model />
 
-        <OrbitControls />
-      </Canvas>
+          <OrbitControls />
+        </Canvas>
+      </div>
+
+
+      <button onClick={takeScreenshot}>Save image</button>
 
     </>
   )
