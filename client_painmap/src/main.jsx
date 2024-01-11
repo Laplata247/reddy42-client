83db949f609f4576b169a5fe81451b8348ba0023
laplata247 <enquiriesmbp@gmail.com>
2024-01-11 16:35:18 +0000

    commit

--- client_painmap/src/main.jsx
+++ client_painmap/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
+import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ConsultationsProvider } from './components/contexts'
 import { GenderProvider } from './components/contexts'
