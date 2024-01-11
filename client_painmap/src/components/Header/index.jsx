32536367f711da8cac234c52868c0d957cd78cb0
booshrat <booshrat@gmail.com>
2024-01-11 23:25:00 +0000

    add translation feature

--- client_painmap/src/components/Header/index.jsx
+++ client_painmap/src/components/Header/index.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <header>
                 <nav>
                     <NavLink to="/">Home</NavLink>
-                    <NavLink to="/select-gender">Map Pain</NavLink>
+                    <NavLink to="/select-gender">Map Your Pain</NavLink>
                     <NavLink to="/chat">Chat</NavLink>
                     <NavLink to="/history">Medical History</NavLink>
                 </nav>
