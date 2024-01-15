37d1d982751a304ec19f19ea7368eb20440c3282
David <david@dmfraser.com>
2024-01-15 16:21:28 +0000

    tests for menu done mostly

---
+++ client_painmap/src/components/Model/Model.test.jsx
@@ -0,0 +1,11 @@
+import ReactThreeTestRenderer from '@react-three/test-renderer'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { screen, render, cleanup, fireEvent } from "@testing-library/react";
+import {Model} from '.'
+
+test('mesh is rendered', async () => {
+    const renderer = await ReactThreeTestRenderer.create(<Model />)
+    expect(renderer).toBeInTheDocument()
+  })
--- client_painmap/src/components/Stickers/Sticker.test.jsx
+++ client_painmap/src/components/Stickers/Sticker.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { describe, it, expect, beforeEach, afterEach } from "vitest";
-import { screen, render, cleanup } from "@testing-library/react";
+import { screen, render, cleanup, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import * as matchers from "@testing-library/jest-dom/matchers";
 expect.extend(matchers);
@@ -17,7 +17,31 @@ describe("Navbar Component", () => {
   it("displays menu when the page is rendered", () => {
     const nav = screen.getByRole("menu");
     expect(nav).toBeInTheDocument();
-    expect(nav.childNodes.length).toBe(2);
+    expect(nav.childNodes.length).toBe(6);
   });
+
+  it("opens the menue when clicking on the arrow", () => {
+    const nav = screen.getByRole("menu");
+    const arrow = screen.getByRole("arrow");
+    expect(arrow).toBeInTheDocument();
+    fireEvent.click(arrow)
+    expect(nav.id).toBe("stickerSelectorClosed");
+    fireEvent.click(arrow)
+    expect(nav.id).toBe("stickerSelectorOpen");
+  });
+
+  it("changes the active sticker when clicking on one", () => {
+    const sticker = screen.getByRole("sticker")
+    expect(sticker.className).toBe("inactive");
+    fireEvent.click(sticker)
+    expect(sticker.className).toBe("active")
+  })
+
+  it("changes the size of the sticker when the bar is moved", () => {
+    const scale = screen.getByRole("scale")
+    fireEvent.change(scale)
+
+  })
+
 
 });
