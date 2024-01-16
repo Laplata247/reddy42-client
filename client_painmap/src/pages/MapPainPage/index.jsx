73c7bb1f022721b5bf0a94541205d4679d3ae554
laplata247 <enquiriesmbp@gmail.com>
2024-01-16 15:07:56 +0000

    styled mappainpage

--- client_painmap/src/pages/MapPainPage/index.jsx
+++ client_painmap/src/pages/MapPainPage/index.jsx
@@ -1,56 +1,61 @@
 import React, { useState, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, RandomizedLight, AccumulativeShadows, Environment } from '@react-three/drei';
-import { DecalGeometry } from 'three/addons/geometries/DecalGeometry.js'; // Import decal geometry package
+import { DecalGeometry } from 'three/addons/geometries/DecalGeometry.js'; //import decal geometry pakage 
 import html2canvas from 'html2canvas';
 import { Model, Stickers, PainForm } from '../../components';
 
 import './style.css';
 
 const MapPainPage = () => {
-  const [drawingEnabled, setDrawingEnabled] = useState(false);
-  const [decals, setDecals] = useState([]);
-  const [img, setImg] = useState('');
-  const [sticker, setSticker] = useState('src/assets/dot.png');
-  const [scaleMod, setScaleMod] = useState(1);
-  const [visible, setVisible] = useState(false);
 
-  const canvasRef = useRef(null);
+  const [drawingEnabled, setDrawingEnabled] = useState(false);
+  const [decals, setDecals] = useState([])
+  const [img, setImg] = useState('');
+  const [sticker, setSticker] = useState('src/assets/dot.png')
+  const [scaleMod, setScaleMod] = useState(1)
+  const [visible, setVisible] = useState(false)
 
   function togglePopup() {
     takeScreenshot();
     setVisible(!visible);
-  }
+  };
 
   const toggleDrawing = () => {
     setDrawingEnabled(!drawingEnabled);
   };
 
   const takeScreenshot = () => {
-    const element = canvasRef.current;
-    if (!element) return;
-
+    const element = document.getElementById("canvasDiv");
     html2canvas(element).then((canvas) => {
-      let image = canvas.toDataURL('image/jpeg');
-      setImg(image);
-    });
-  };
-
+      let image = canvas.toDataURL("image/jpeg");
+      setImg(image)
+      // console.log(image)
+      // const a = document.createElement("a")
+      // a.href = image
+      // a.download = "screenshot.jpeg"
+      // a.click()
+    })
+  }
   const removeDecal = () => {
-    const newDecals = decals.slice(0, -1);
-    setDecals(newDecals);
-  };
+    const newDecals = decals.slice(0, -1)
+    setDecals(newDecals)
+
+  }
+
+
 
   return (
-    <>
+<>
       <h1>Map Pain Page</h1>
 
-      <div className="center-container">
-        <Canvas
-          ref={canvasRef}
+
+      <div class="center-container">
+      
+        <Canvas id='canvasDiv'
           gl={{
-            preserveDrawingBuffer: true, // allow showing model on the screenshot
+            preserveDrawingBuffer: true // allow showing model on the screenshot
           }}
           style={{
             height: '500px',
@@ -58,12 +63,15 @@ const MapPainPage = () => {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            // background: 'white',
+            // border: '2px solid Green'
           }}
-          shadows
+        shadows
           camera={{ position: [0, 0, 2] }}
         >
+
           <ambientLight intensity={0.5} />
-          <spotLight position={[1, 5, -1]} angle={0.45} penumbra={1} />
+        <spotLight position={[1, 5, -1]} angle={0.45} penumbra={1} />
           <directionalLight
             position={[5, 5, 1]}
             intensity={3}
@@ -71,19 +79,24 @@ const MapPainPage = () => {
             shadow-mapSize-width={1024}
             shadow-mapSize-height={1024}
           />
-          <Model decals={decals} setDecals={setDecals} sticker={sticker} scaleMod={scaleMod} />
+          <Model decals={decals} setDecals={setDecals} sticker={sticker} scaleMod={scaleMod}/>
+        {/* <color attach="background" args={["lightpink"]} /> */}
+          {/* <RandomizedLight amount={8} radius={10} ambient={0.5} position={[2.5, 5, -5]} bias={0.001} /> */}
           <OrbitControls enableRotate={true} enablePan={true} enableZoom={true} />
         </Canvas>
-        <div id="button-container">
-          <button onClick={togglePopup}>Save image</button>
+
+        {/* <button onClick={togglePopup}>Save image</button>
+        {visible ? <PainForm toggle={togglePopup} image={img} /> : null} */}
+        
+        <Stickers setSticker={setSticker} setScaleMod={setScaleMod} />
+      
+      <div id="button-container">
+      <button onClick={togglePopup}>Save image</button>
+        {visible ? <PainForm toggle={togglePopup} image={img} /> : null}
           <button onClick={removeDecal}>Undo</button>
         </div>
-
-        {visible ? <PainForm toggle={togglePopup} image={img} /> : null}
-
-        <Stickers setSticker={setSticker} setScaleMod={setScaleMod} />
-      </div>
-    </>
+        </div>
+</>
   );
 };
 
