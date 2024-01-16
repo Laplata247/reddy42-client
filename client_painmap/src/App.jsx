2e7112e7d946b5df3a0001e2a95488d61e8ebe25
laplata247 <enquiriesmbp@gmail.com>
2024-01-16 11:07:29 +0000

    commit

--- client_painmap/src/App.jsx
+++ client_painmap/src/App.jsx
@@ -49,23 +49,24 @@ function App() {
     <>
     <Routes>
       <Route path="/landing" element={<Pages.LandingPage />} />
-
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Pages.HomePage />} />
-
-        <Route path="/select-gender" element={user ? <Pages.GenderSelection /> : < Pages.HomePage />} />
-        <Route path="/painmap" element={user ? <Pages.MapPainPage /> : < Pages.HomePage />} />
-
-        <Route path="/chat" element={user ? <Pages.ChatPage /> : < Pages.HomePage />} />
-        <Route path="/history">
-          <Route index element={user ? <Pages.MedicalHistoryPage /> : < Pages.HomePage />} />
-          <Route path=":id" element={user ? <Pages.ConsultationPage /> : < Pages.HomePage />} />
-        </Route>
-      </Route>
-
       <Route path="/signup" element={<Pages.SignupPage />} />
       <Route path="/login" element={<Pages.Login />} />
-      <Route path="*" element={<Pages.NotFoundPage />} />
+
+      {showHeader && (
+        <Route path="/" element={<Header />}>
+          <Route index element={<Pages.HomePage />} />
+
+          <Route path="/select-gender" element={<Pages.GenderSelection />} />
+          <Route path="/painmap" element={<Pages.MapPainPage />} />
+
+          <Route path="/chat" element={<Pages.ChatPage />} />
+          <Route path="/history">
+            <Route index element={<Pages.MedicalHistoryPage />} />
+            <Route path=":id" element={<Pages.ConsultationPage />} />
+          </Route>
+          <Route path="*" element={<Pages.NotFoundPage />} />
+        </Route>
+      )}
     </Routes>
     </>
   );
