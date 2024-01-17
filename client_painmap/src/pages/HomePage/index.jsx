83c328b7ede9943dee7f5015c03171f7f7505b31
laplata247 <enquiriesmbp@gmail.com>
2024-01-17 21:40:31 +0000

    styling homepage

--- client_painmap/src/pages/HomePage/index.jsx
+++ client_painmap/src/pages/HomePage/index.jsx
@@ -4,9 +4,9 @@ import './styles.css';
 
 const HomePage = () => {
   return (
-    <div role='PageWrapper'className='home'>
-      <div className='PageWrapper'>
-        <h1>Welcome back, name!</h1>
+    <div role='PageWrapper' className='home'>
+      <h1>Welcome back!</h1>
+      <div className='PageWrapper'> {/* This div will now use the grid layout */}
         <Link to="/chat">
           <div className="chat">
             <p>Chat with GP</p>
@@ -22,9 +22,9 @@ const HomePage = () => {
             <p>Articulate Pain</p>
           </div>
         </Link>
-        <div className="images">
+        {/* <div className="images">
           <p>Past Images</p>
-        </div>
+        </div> only invoke for experimental */}
       </div>
     </div>
   );
