750b76a3386f2279d799cbc066083b0fb35830d6
laplata247 <enquiriesmbp@gmail.com>
2024-01-18 13:40:15 +0000

    styled home page

--- client_painmap/src/components/Header/index.jsx
+++ client_painmap/src/components/Header/index.jsx
@@ -1,59 +1,63 @@
-import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
-import { Link } from "react-router-dom";
-// import { Logo } from "../Logo";
+import React from 'react';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './style.css';
 
-import { useLogout } from "../../hooks/useLogout";
-import { useAuthContext } from "../../hooks/useAuthContext";
-
+import { useLogout } from '../../hooks/useLogout';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const Header = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const location = useLocation();
 
-    const { logout } = useLogout();
-    const { user } = useAuthContext();
+  const handleClick = () => {
+    logout();
+  };
 
-    const handleClick = () => {
-        logout();
-    };
-    return (
-        <main>
-            <header id='navigationMenu'>
-            <Link to="/">
-                <img src='../../logo.png' className='nav-logo'></img>
-            </Link>
-                <nav className='navbar'>
-                    {user && (
-                        <div className="userDisplay">
-                            <NavLink to="/">Home</NavLink>
-                            {!(user.user_id =="NHS@email.com") && (
-                                <>
-                                    <NavLink to="/select-gender">Articulate Your Pain</NavLink>
-                                    <NavLink to="/history">Medical History</NavLink>
-                                </>)}
-                            <NavLink to="/chat">Chat</NavLink>
-                            {user.user_id =="NHS@email.com" && (
-                                    <NavLink to="/search">Search patients</NavLink>
-                            )}
-                        </div>
-                    )}
-                    {!user && (
-                        <div className="userDisplay">
-                            <Link to="/login">Login</Link>
-                            <Link to="/signup">Signup</Link>
-                        </div>
-                    )}
+  // Check if the current location is the homepage
+  const isHomePage = location.pathname === '/';
 
-                </nav>
-                {user && (
-                    <div className="userDisplay">
-                        <button className='logout-btn' onClick={handleClick}>Log out</button>
-                    </div>
-                )}
-            </header>
-            <Outlet />
-        </main>
-    )
-}
+  return (
+    <main>
+      <header id="navigationMenu">
+        <Link to="/">
+          <img src="../../logo.png" className="nav-logo" alt="Logo" />
+        </Link>
+        <nav className='navbar'>
+            <div className ="links">
+          {user && !isHomePage && (
+            <div className="userDisplay">
+              <NavLink to="/">Home</NavLink>
+              {!(user.user_id === "NHS@email.com") && (
+                <>
+                  <NavLink to="/select-gender">Articulate Your Pain</NavLink>
+                  <NavLink to="/history">Medical History</NavLink>
+                </>
+              )}
+              <NavLink to="/chat">Chat</NavLink>
+              {user.user_id === "NHS@email.com" && (
+                <NavLink to="/search">Search patients</NavLink>
+              )}
+            </div>
+          )}
+          {!user && !isHomePage && (
+            <div className="userDisplay">
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+          </div>
+          {user && (
+            <div className="userDisplay">
+              <button className='logout-btn' onClick={handleClick}>Log out</button>
+            </div>
+          )}
+        </nav>
+      </header>
+      <Outlet />
+    </main>
+  );
+};
 
-export default Header
+export default Header;
